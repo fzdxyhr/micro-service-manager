689bc7ae5ebbdc0a2cb9667f4bf1ef737be4e812
yhr <1361579931@qq.com>
2018-03-22 23:16:45 +0800

    代码修改提交

--- src/main/java/com/yhr/microservice/microservicemanager/MicroServiceManagerApplication.java
+++ src/main/java/com/yhr/microservice/microservicemanager/MicroServiceManagerApplication.java
@@ -2,9 +2,10 @@ package com.yhr.microservice.microservicemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicroServiceManagerApplication {
+public class MicroServiceManagerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceManagerApplication.class, args);
