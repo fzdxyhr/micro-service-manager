06e22afcbe238d77e319040b8fe188e088cb8bd9
yhr <1361579931@qq.com>
2018-03-22 23:14:59 +0800

    代码提交

---
+++ src/main/java/com/yhr/microservice/microservicemanager/MicroServiceManagerApplication.java
@@ -0,0 +1,12 @@
+package com.yhr.microservice.microservicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceManagerApplication.class, args);
+	}
+}
